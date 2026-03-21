8316808fbecce42c4293170bfeaee58bc5c738fb
mathiuxd <matzap.ai@gmail.com>
2026-03-20 22:31:24 -0500

    Fix: The path of the projects in the solution. Add: Entity Roles

---
+++ Core/Meetzy.Domain/Entities/Roles/Roles.cs
@@ -0,0 +1,9 @@
+namespace Meetzy.Domain.Entities.Roles
+{
+    public class Roles
+    {
+        public Guid RoleId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+    }
+}
