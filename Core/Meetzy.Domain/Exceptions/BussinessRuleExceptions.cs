b0d186b31e29fcdbf54495832f2c56d9857b0a29
mathiuxd <matzap.ai@gmail.com>
2026-03-21 21:59:03 -0500

    Add: Constructor and bussiness rule exceptions

---
+++ Core/Meetzy.Domain/Exceptions/BussinessRuleExceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meetzy.Domain.Exceptions
+{
+    public class BussinessRuleExceptions : Exception
+    {
+        public BussinessRuleExceptions(string message) : base(message) 
+        {
+
+        }
+    }
+}
