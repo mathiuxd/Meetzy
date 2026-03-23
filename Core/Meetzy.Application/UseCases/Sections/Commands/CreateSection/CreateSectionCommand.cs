cbf3d287b7c612d481df312018c9331e215648e2
mathiuxd <matzap.ai@gmail.com>
2026-03-23 17:00:49 -0500

    Add: Basic orchestration for the project NOTE: Still needs to be finished

---
+++ Core/Meetzy.Application/UseCases/Sections/Commands/CreateSection/CreateSectionCommand.cs
@@ -0,0 +1,9 @@
+using Meetzy.Application.Utilities.Mediator;
+
+namespace Meetzy.Application.UseCases.Sections.Commands.CreateSection
+{
+    public class CreateSectionCommand : IRequest<Guid>
+    {
+        public required string Name { get; set; }
+    }
+}
