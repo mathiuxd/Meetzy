52314e945010784cca8839f825b699b290756424
hxro0ld <haroldechavarria54@gmail.com>
2026-04-15 13:51:06 -0500

    Se agrega configuracion de CommunityMenbers

---
+++ Infrastructure/Meetzy.Persistence/Configurations/CommunityMembersConfiguration.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Meetzy.Domain;
+
+namespace Meetzy.Persistence.Configurations
+{
+    public class CommunityMembersConfiguration : IEntityTypeConfiguration<CommunityMembers>
+    {
+        public void Configure(EntityTypeBuilder<CommunityMembers> builder)
+            {
+            builder.ToTable("CommunityMembers");
+            builder.HasKey(cm => cm.Id);
+            builder.Property(cm => cm.Id).ValueGeneratedNever();
+            builder.Property(cm => cm.JoinedAt)
+                .IsRequired();
+            // Relationships
+            builder.HasOne(cm => cm.Community)
+                .WithMany(c => c.Members)
+                .HasForeignKey(cm => cm.CommunityId)
+                .OnDelete(DeleteBehavior.Cascade);
+            builder.HasOne(cm => cm.User)
+                .WithMany(u => u.CommunityMemberships)
+                .HasForeignKey(cm => cm.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
