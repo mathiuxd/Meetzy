5f244bca0d0f2113fc12dcec5c03df6d9de6ea39
hxro0ld <haroldechavarria54@gmail.com>
2026-04-15 01:04:47 -0500

    Se implementa PersistenceServicesRegistry para configurar DI con DbContext, UnitOfWork y repositorio de secciones

---
+++ Infrastructure/Meetzy.Persistence/Repositories/PersistenceServicesRegistry.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Meetzy.Application.Contracts.Persistence;
+using Meetzy.Application.Contracts.Repositories;
+using Meetzy.Persistence.Repositories;
+using Meetzy.Persistence.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meetzy.Persistence.Repositories
+{
+	public static class PersistenceServicesRegistry
+	{
+		public static IServiceCollection AddPersistenceRepositories(this IServiceCollection services)
+		{
+			services.AddDbContext<MeetzyDbContext>(options =>
+			{
+				options.UseSqlServer("name=MeetzyConnectionString");
+			});
+
+			services.AddScoped<IUnitOfWork, EfCoreUnitOfWork>();
+			services.AddScoped<ISectionsRepository, SectionsRepository>();
+			return services;
+		}
+	}
+}
